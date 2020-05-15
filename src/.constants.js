a9ffef7ac37ddbee1cb6a0e31e9bc83278cd6e83
hypercianstand <65367898+hypercianstand@users.noreply.github.com>
2020-05-15 10:42:39 +0430

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,12 +1,12 @@
 module.exports = Object.freeze({
-  TOKEN: '1131082763:AAHxVvtGcEKPR7VPEHATeH5iMGL-FWPeRio',
-  ARIA_SECRET: '1131082763:AAHxVvtGcEKPR7VPEHATeH5iMGL-FWPeRio',
+  TOKEN: '1109730832:AAFOUjmJivKJSEPMYJMZn97t-c3l4VA82Lk',
+  ARIA_SECRET: '917707263:AAH9kfRGuz23FJC1NsLAcqD1I2_-GaUPP3s',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY', 'HDCAM', 'CAMRIP', '1xbet', 'preDVD', 'predvdrip', '.php', '.html'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['YIFY', 'HDCAM', 'CAMRIP', '1xbet', 'preDVD', 'predvdrip', '.php', '.html'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  INDEX_DOMAIN: 'https://hypercianclash.veis.workers.dev/001/', //Get it from here https://gdindex-code-builder.glitch.me/
+  INDEX_DOMAIN: 'https://hypercianclash.veis.workers.dev/002/', //Get it from here https://gdindex-code-builder.glitch.me/
   GDRIVE_PARENT_DIR_ID: '1K2cFkrIejrLMtGrog7HYplKwbF8ba3pl',
   SUDO_USERS: [253131957],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [-1001446657400],	// Telegram chat IDs. Anyone in these chats can use the bot.
