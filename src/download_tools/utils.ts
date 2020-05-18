cd472317c072c9d333e5c75141d724427c840bf0
hypercianstand <65367898+hypercianstand@users.noreply.github.com>
2020-05-18 23:59:15 +0430

    Update utils.ts

--- src/download_tools/utils.ts
+++ src/download_tools/utils.ts
@@ -146,7 +146,7 @@ export function generateStatusMessage(totalLength: number, completedLength: numb
   var speedStr = formatSize(speed);
   var eta = downloadETA(totalLength, completedLength, speed);
   var type = isUploading ? '✈️ Uploading ' : '✏️ File Name';
-  var message = `<b>${type}</b> : <code>${fileName}</code>\n<b>🛒 Size</b> : <code>${totalLengthStr}</code>\n<b>💸 Progress</b> : <code>${progressString}</code>\n<b>⚡️ Speed</b> : <code>${speedStr}ps</code>\n<b>⌛️ ETA</b> : <code>${eta}</code>`;
+  var message = `<b>${type}</b> : <code>${fileName}</code>\n<b>Size</b> : <code>${totalLengthStr}</code>\n<b>Progress</b> : <code>${progressString}</code>\n<b>⚡️ Speed</b> : <code>${speedStr}ps</code>\n<b>ETA</b> : <code>${eta}</code>`;
   var status = {
     message: message,
     filename: fileName,
