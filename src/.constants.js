03ca294898e71b890d52fcc69b19c38067e53c6b
hypercianstand <65367898+hypercianstand@users.noreply.github.com>
2020-05-19 20:57:26 +0430

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,15 +1,15 @@
 module.exports = Object.freeze({
-  TOKEN: '1215165039:AAGmPMnAf1yTAI_bDMY8id-NiimTvNl8FDE',
+  TOKEN: '1287874148:AAE3b0d_k5F-beuQ-Sj6S8OToNAd4sQlhHc',
   ARIA_SECRET: '917707263:AAH9kfRGuz23FJC1NsLAcqD1I2_-GaUPP3s',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  INDEX_DOMAIN: 'https://hiddenkiano.veis.workers.dev/003/', //Get it from here https://gdindex-code-builder.glitch.me/
-  GDRIVE_PARENT_DIR_ID: '1-0QTDh50JbuRJMrMhjJrnLpu0OccWaAA',
+  INDEX_DOMAIN: 'https://hyhadi.veis.workers.dev/005/', //Get it from here https://gdindex-code-builder.glitch.me/
+  GDRIVE_PARENT_DIR_ID: '1dnQrepDbN2xpjG-FDmfrAgqHg6HNX4QD',
   SUDO_USERS: [253131957],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [-1001291016119],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  AUTHORIZED_CHATS: [-1001346238977],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
@@ -23,7 +23,7 @@ module.exports = Object.freeze({
   },
   COMMANDS_USE_BOT_NAME: {
     ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command
-    NAME: "@ARiA_CLouD_004_BoT"
+    NAME: "@ARiA_CLouD_005_BoT"
   },
   IS_TEAM_DRIVE: true
 });
